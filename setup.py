988d1591a6271e16903dcc88c5d593579b269036
Benjie Chen <benjie@ginkgobioworks.com>
2014-08-27 11:29:31 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from setuptools import setup
 
-setup(name='giraffe_features',
+setup(name='giraffe-features',
       version='3.0',
       description='Giraffe compatible biological feature description',
       author='Benjie Chen',
       author_email='benjie@alum.mit.edu',
-      packages=["."],
-      package_dir={"": "."},
+      packages=['giraffe_features'],
+      package_dir={"giraffe_features": "."},
       install_requires=[])
